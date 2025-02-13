f60349c7b68c90436cc7e22066959e400e9dfe72
Petar Vladov <60221587+petar1905@users.noreply.github.com>
2025-02-13 09:27:38 +0200

    added bootstrap

---
+++ src/components/RobotCard.tsx
@@ -0,0 +1,12 @@
+export default function RobotCard() {
+  return (
+    <div className="card" style={{width: "18rem"}}>
+      <img src="/misty.avif" className="card-img-top" alt="..."></img>
+      <div className="card-body text-center">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  )
+}
